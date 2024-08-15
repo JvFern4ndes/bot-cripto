e755392ef171f81e453f76693039cfe8a6c10b6a
João <jvfernandes.contato@gmail.com>
2024-08-15 13:38:56 -0300

    feat: Monitoring the price of bitcoin with the start function

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const SYMBOL = "BTCUSDT";
 const BUY_PRICE = 34160;
 const SELL_PRICE = 34501;
 
+const API_URL = "https://testnet.binance.vision"; //https://api.binance.com
+
 async function start() {
-    // comandos do robo
-    console.log("start");
+    const { data } = await axios.get(API_URL + "/api/v3/klines?limit=21&interval=15m&symbol=" + SYMBOL);
+    const candle = data[data.length -1];
+    const price = parseFloat(candle[4]);
+
+    console.clear();
+    console.log("Price:" + price);
 }
 
 setInterval(start, 3000);
