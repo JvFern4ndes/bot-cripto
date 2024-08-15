cf108ff527b080e7ddf1cac570144069f873dacb
João <jvfernandes.contato@gmail.com>
2024-08-15 13:47:30 -0300

    feat: Implementing the asset purchase and sale logic in the start function

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const SELL_PRICE = 34501;
 
 const API_URL = "https://testnet.binance.vision"; //https://api.binance.com
 
+let isOpened = false;
+
 async function start() {
     const { data } = await axios.get(API_URL + "/api/v3/klines?limit=21&interval=15m&symbol=" + SYMBOL);
     const candle = data[data.length -1];
@@ -13,6 +15,17 @@ async function start() {
 
     console.clear();
     console.log("Price:" + price);
+
+    if(price <= BUY_PRICE && isOpened === false) {
+        console.log("comprar");
+        isOpened = true;
+    }
+    else if(price >= SELL_PRICE && isOpened === true) {
+        console.log("vender");
+        isOpened = false;
+    }
+    else
+        console.log("aguardar");
 }
 
 setInterval(start, 3000);
