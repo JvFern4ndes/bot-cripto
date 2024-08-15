680af9f06777b07db2bb482a6e1d11436fca2a68
João <jvfernandes.contato@gmail.com>
2024-08-15 13:18:37 -0300

    feat: creating the start function and setting a time interval for it to be executed

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const axios = require('axios');
 const SYMBOL = "BTCUSDT";
 const BUY_PRICE = 34160;
 const SELL_PRICE = 34501;
+
+async function start() {
+    // comandos do robo
+    console.log("start");
+}
+
+setInterval(start, 3000);
+
+start();
